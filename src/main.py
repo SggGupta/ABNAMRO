a6c8adc59e473712b43cf689e4ce2ccbc71bb7c6
Sapna Gupta <sapnagupta@Sapnas-MBP-2.home>
2024-05-06 08:56:33 +0200

    File path changedin main

--- src/main.py
+++ src/main.py
@@ -82,19 +82,19 @@ def run (spark, path):
     df2 = df2.alias('df2')
     df3 = df3.alias('df3')
 
-    path1 = 'file:///Users/sapnagupta/Downloads/latest/output/it_data'
+    path1 = './output/it_data'
     it_data(df1, df2, path1)
-    path2 = 'file:///Users/sapnagupta/Downloads/latest/output/marketing_address_info'
+    path2 = './output/marketing_address_info'
     marketing_address_info(df1, df2,path2)   
-    path3 = 'file:///Users/sapnagupta/Downloads/latest/output/department_breakdown'
+    path3 = './output/department_breakdown'
     output3 = department_breakdown(df1, df2,path3)
-    path4 =  'file:///Users/sapnagupta/Downloads/latest/output/top_3_best_performers_per_department'  
+    path4 =  './output/top_3_best_performers_per_department'  
     top_3_best_performers_per_department(df1,df2,path4)
-    path5 = 'file:///Users/sapnagupta/Downloads/latest/output/top_3_most_sold_per_department_netherlands'
+    path5 = './output/top_3_most_sold_per_department_netherlands'
     top_3_most_sold_products_per_department(df1,df3,path5)
-    path6 = 'file:///Users/sapnagupta/Downloads/latest/output/best_salesperson'
+    path6 = './output/best_salesperson'
     top_3_best_salesperson_department(df2,df3,path6)
-    path7 = 'file:///Users/sapnagupta/Downloads/latest/output/best_salesperson_department'
+    path7 = './output/best_salesperson_department'
     top_3_best_salesperson_department_country(df1,df2,df3,path7)
 
 if __name__ == '__main__':
