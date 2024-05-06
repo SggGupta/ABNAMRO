dcd60b38bf251438c712a72f1284ec631c9e5525
Sapna Gupta <sapnagupta@Sapnas-MBP-2.home>
2024-05-06 13:42:27 +0200

    logging

--- src/assignment/sales_data.py
+++ src/assignment/sales_data.py
@@ -1,6 +1,9 @@
 import argparse
 import pyspark
 from pyspark.sql import SparkSession
+import logging
+from logging import Logger
+from typing import Dict, Any, Optional
 from src.assignment.main import it_data
 from src.assignment.main import marketing_address_info
 from src.assignment.main import department_breakdown
@@ -8,6 +11,20 @@ from src.assignment.main import top_3_best_performers_per_department
 from src.assignment.main import top_3_most_sold_products_per_department
 from src.assignment.main import top_3_best_salesperson_department
 from src.assignment.main import top_3_best_salesperson_department_country
+
+
+class LoggerProvider:
+    def get_logger(self, spark: SparkSession, custom_prefix: Optional[str] = ""):
+        log4j_logger = spark._jvm.org.apache.log4j  # noqa
+        return log4j_logger.LogManager.getLogger(custom_prefix + self.__full_name__())
+
+    def __full_name__(self):
+        klass = self.__class__
+        module = klass.__module__
+        if module == "__builtin__":
+            return klass.__name__  # avoid outputs like '__builtin__.str'
+        return module + "." + klass.__name__
+
 
 def run (spark, path):  
     df1 = spark.read.csv(path+'/dataset_one.csv',header='true', inferSchema='true')
@@ -32,6 +49,7 @@ def run (spark, path):
     top_3_best_salesperson_department(df2,df3,path6)
     path7 = './output/best_salesperson_department'
     top_3_best_salesperson_department_country(df1,df2,df3,path7)
+    logging.debug("Debug message: END of the application")
 
 if __name__ == '__main__':
 
@@ -42,9 +60,11 @@ if __name__ == '__main__':
     args = parser.parse_args()
     print(args.path)
     print('User path is -> {}'.format(args.path))
+    logging.info("ABN AMRO assignment")
+    logging.debug("Debug message: START of the application")
     spark = SparkSession \
         .builder \
         .appName(args.path) \
         .getOrCreate()
-    spark.sparkContext.setLogLevel("WARN")
+    spark.sparkContext.setLogLevel("DEBUG")
     run(spark,args.path)
